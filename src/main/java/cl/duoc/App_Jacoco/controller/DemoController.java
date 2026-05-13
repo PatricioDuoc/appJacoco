7e175c9b164b51b2571562b7a200e0ed58c50938
PatricioDuoc <pa.fuenzalidao@profesor.duoc.cl>
2026-05-13 14:48:03 -0400

    se agrega pipeline

--- src/main/java/cl/duoc/App_Jacoco/controller/DemoController.java
+++ src/main/java/cl/duoc/App_Jacoco/controller/DemoController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoController {
     @GetMapping("/saludo")
+
+    // se agrega pipeline
     public String saludo(){
         return "saludos desde Springboot";
     }
